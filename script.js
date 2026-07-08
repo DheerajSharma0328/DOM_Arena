9ec0561a98e8fa00795c0633c1c7eff18241bd4c
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-07-08 20:43:23 +0530

    DOM Practice

---
+++ script.js
@@ -0,0 +1,20 @@
+let doc = document.documentElement;
+let hed = document.head;
+let bod = document.body;
+
+let bodfirst = bod.firstChild;
+let bodlast = bod.lastChild;
+let bodchildren = bod.children;
+let docc = document;
+
+let idd = document.getElementById('parent');
+console.log(idd);
+
+
+// console.log(docc);
+// console.log(doc);
+// console.log(hed);
+// console.log(bod);
+// console.log('Body First Child: ' , bodfirst);
+// console.log('Body Last Child: ' , bodlast);
+// console.log('Body Children: ' , bodchildren);
